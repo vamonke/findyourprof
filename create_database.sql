adb8d218b4e2a46b75a61cd957e4b7f1f6968b07
Varick Lim <valishru@hotmail.com>
2019-05-09 20:04:37 +0800

    Create student on login and store id in session

--- create_database.sql
+++ create_database.sql
@@ -1,11 +1,30 @@
-CREATE DATABASE findyourprof;
+CREATE DATABASE IF NOT EXISTS findyourprof;
 USE findyourprof;
-CREATE TABLE prof (id varchar(255) PRIMARY KEY, name varchar(255), school varchar(255), 
-createdAt timestamp NOT NULL default CURRENT_TIMESTAMP);
-CREATE TABLE student (id varchar(255) PRIMARY KEY, email varchar(255), 
-createdAt timestamp NOT NULL default CURRENT_TIMESTAMP);
-CREATE TABLE review (id varchar(255) PRIMARY KEY, rating float(1,1), comment text(1000), advice text(500), meetup text(100),
-studentId varchar(255), profId varchar(255),
-createdAt timestamp NOT NULL default CURRENT_TIMESTAMP,
-CONSTRAINT reviewFK1 FOREIGN KEY (studentId) REFERENCES student(id),
-CONSTRAINT reviewFK2 FOREIGN KEY (profId) REFERENCES prof(id));
+
+DROP TABLE IF EXISTS prof;
+CREATE TABLE prof (
+  id varchar(255) PRIMARY KEY,
+  name varchar(255), school varchar(255), 
+  createdAt timestamp NOT NULL default CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+  id varchar(255) PRIMARY KEY,
+  email varchar(255) NOT NULL,
+  createdAt timestamp NOT NULL default CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS review;
+CREATE TABLE review (
+  id varchar(255) PRIMARY KEY,
+  rating float(2,1),
+  comment text(1000),
+  advice text(500),
+  meetup text(100),
+  userId varchar(255),
+  profId varchar(255),
+  createdAt timestamp NOT NULL default CURRENT_TIMESTAMP
+  -- CONSTRAINT reviewFK1 FOREIGN KEY (userId) REFERENCES user(id),
+  -- CONSTRAINT reviewFK2 FOREIGN KEY (profId) REFERENCES prof(id)
+);
