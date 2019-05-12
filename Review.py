00031a401ad6db8200a18dc72d2b8162fb351de9
Varick Lim <valishru@hotmail.com>
2019-05-12 22:02:07 +0800

    Move review methods into another file

--- Review.py
+++ Review.py
@@ -20,7 +20,7 @@ def getMeetup(days):
     else:
         return None
 
-def get_prof(profId):
+def get_reviews():
     conn = pymysql.connect(
         db='findyourprof',
         user='root',
@@ -28,50 +28,42 @@ def get_prof(profId):
         host='localhost')
 
     c = conn.cursor()
-    c.execute("SELECT * FROM prof WHERE id = %s;", (profId))
-    row = c.fetchone()
+    c.execute("SELECT * from review;")
 
-    if (len(row) == 0):
-        c.close()
-        conn.close()
-        return None
-
-    prof = {
-        'id': row[0],
-        'name': row[1],
-        'school': row[2]
-    }
-    
-    query = "SELECT ROUND(AVG(rating), 1) AS rating, AVG(meetup) AS meetup FROM review WHERE profId = '%s';" % (prof['id'])
-    print(query)
-
-    c.execute(query)
-    row = c.fetchone()
-    if (row):
-        prof['rating'] = row[0]
-        prof['meetup'] = getMeetup(row[1])
-    c.close()
-    conn.close()
-    return prof
-
-
-def get_profs():
-    conn = pymysql.connect(
-        db='findyourprof',
-        user='root',
-        passwd='',
-        host='localhost')
-
-    c = conn.cursor()
-    c.execute("SELECT * from prof;")
-
-    profs = [{
-        'id': row[0],
-        'name': row[1],
-        'school': row[2],
+    reviews = [{
+        'review_id': row[0],
+        'rating': row[1],
+        'comment': row[2],
+        'advice': row[3],
+        'meetup': row[4],
+        'studentId': row[5],
+        'profId': row[6]
     } for row in c.fetchall()]
 
     c.close()
     conn.close()
-    return profs
+    return reviews
 
+def get_prof_reviews(profId):
+    conn = pymysql.connect(
+        db='findyourprof',
+        user='root',
+        passwd='',
+        host='localhost')
+
+    c = conn.cursor()
+    c.execute("SELECT * from review where profId = %s;", (profId))
+
+    prof_reviews = [{
+        'review_id': row[0],
+        'rating': row[1],
+        'comment': row[2],
+        'advice': row[3],
+        'meetup': getMeetup(row[4]),
+        'studentId': row[5],
+        'profId': row[6]
+    } for row in c.fetchall()]
+
+    c.close()
+    conn.close()
+    return prof_reviews
--- api.py
+++ api.py
@@ -8,50 +8,40 @@ import googleapiclient.discovery
 
 import google_auth
 import Prof
+import Review
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.secret_key = os.environ.get("SECRET_KEY") or "1234567"
 app.register_blueprint(google_auth.app)
 
-
 @app.route('/', methods=['GET'])
 def home():
+    user = None
     if google_auth.is_logged_in():
         user = google_auth.get_current_user()
-        return render_template('index.html', user=user)
-    return render_template('index.html')
-
+    return render_template('index.html', user=user)
 
 @app.route('/review', methods=['GET'])
 def view_review():
-    conn = pymysql.connect(
-        db='findyourprof',
-        user='root',
-        passwd='',
-        host='localhost')
-
-    c = conn.cursor()
-    c.execute("SELECT * from review;")
-
-    prof_reviews = [{
-        'review_id': row[0],
-        'rating': row[1],
-        'comment': row[2],
-        'advice': row[3],
-        'meetup': row[4],
-        'studentId': row[5],
-        'profId': row[6]
-    } for row in c.fetchall()]
-
-    c.close()
-    conn.close()
-
+    reviews = Review.get_reviews()
+    user = None
     if google_auth.is_logged_in():
         user = google_auth.get_current_user()
-        return render_template('review.html', reviews=prof_reviews, user=user)
-    return render_template('index.html')
+    return render_template('review.html', reviews=reviews, user=user)
 
+@app.route('/prof/<prof_id>', methods=['GET'])
+def view_prof(prof_id):
+    prof = Prof.get_prof(prof_id)
+    if (prof == None):
+        return redirect('/')
+    reviews = Review.get_prof_reviews(prof_id)
+    user = None
+    if google_auth.is_logged_in():
+        user = google_auth.get_current_user()
+    return render_template('prof.html', prof=prof, user=user, reviews=reviews)
+
+# API endpoints
 
 @app.route('/api/profs', methods=['GET'])
 def get_profs():
@@ -63,42 +53,9 @@ def get_prof(prof_id):
     prof = Prof.get_prof(prof_id)
     return jsonify(prof)
 
-@app.route('/prof/<prof_id>', methods=['GET'])
-def prof(prof_id):
-    prof = Prof.get_prof(prof_id)
-    if (prof == None):
-        return redirect('/')
-    reviews = Prof.get_prof_reviews(prof_id)
-    user = None
-    if google_auth.is_logged_in():
-        user = google_auth.get_current_user()
-    
-    return render_template('prof.html', prof=prof, user=user, reviews=reviews)
-
 @app.route('/api/review', methods=['GET'])
 def get_review():
-    conn = pymysql.connect(
-        db='findyourprof',
-        user='root',
-        passwd='',
-        host='localhost')
-
-    c = conn.cursor()
-    c.execute("SELECT * from review;")
-
-    prof_reviews = [{
-        'review_id': row[0],
-        'rating': row[1],
-        'comment': row[2],
-        'advice': row[3],
-        'meetup': row[4],
-        'studentId': row[5],
-        'profId': row[6]
-    } for row in c.fetchall()]
-
-    c.close()
-    conn.close()
-    return jsonify(prof_reviews)
-
+    reviews = Review.get_reviews()
+    return jsonify(reviews)
 
 app.run()
